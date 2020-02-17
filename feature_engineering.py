9a07b2b703131345c2e32824322264950ec716fe
Levi Joseph <levi.m.joseph@gmail.com>
2020-02-17 17:05:05 -0600

    added initial files

---
+++ feature_engineering.py
@@ -0,0 +1,5 @@
+# Author: Levi Joseph
+# Purpose: python functionality for feature engineering
+# Create Date: February 17th, 2020
+# Last Update: February 17th, 2020
+
---
+++ visuals.py
@@ -0,0 +1,4 @@
+# Author: Levi Joseph
+# Purpose: python functionality for easy visualization of data
+# Create Date: February 17th, 2020
+# Update Date: February 17th, 2020
